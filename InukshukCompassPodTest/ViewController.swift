16d477b56569cd0b4f17d8315d330605a9ab5fb9
Inukshuk Software, LLC <mark@inuksoft.com>
2019-10-23 14:16:15 -0700

    Initial Commit

---
+++ InukshukCompassPodTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InukshukCompassPodTest
+//
+//  Created by Mark on 10/23/19.
+//  Copyright © 2019 Inukshuk, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
